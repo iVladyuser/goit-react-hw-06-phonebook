0e3810c95cde420df307d63f3a38ff01c532fabe
iVladyuser <korol.v09@gmail.com>
2023-11-16 23:33:51 +0100

    update my code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,6 @@
-import { nanoid } from 'nanoid';
-
 import { ContactForm, ContactList, Filter } from 'components';
-import { useDispatch, useSelector } from 'react-redux';
-import { addContact, deleteContact } from 'redux/contacts/phoneBookSlice';
-import { filterContact } from 'redux/contacts/filterSlice';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(state => state.contactsStore.contacts);
-  const filter = useSelector(state => state.filterStore);
-
-  const handleDeleteContact = contactId => {
-    dispatch(deleteContact(contactId));
-  };
-
-  const handleAddContact = newContact => {
-    const isExist = contacts.some(
-      ({ name }) => name.toLowerCase() === newContact.name.toLowerCase()
-    );
-
-    if (isExist) {
-      alert(`Oops, contact '${newContact.name}' is already in contacts!`);
-      return;
-    }
-    const readyToAddContact = {
-      ...newContact,
-      id: nanoid(),
-    };
-
-    // const addContactAction = {
-    //   type: 'contacts/addContact',
-    //   payload: readyToAddContact,
-    // };
-    dispatch(addContact(readyToAddContact));
-  };
-
-  const changeFilter = value => {
-    dispatch(filterContact(value));
-  };
-
-  const getFilteredContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
   return (
     <div
       style={{
@@ -57,13 +12,10 @@ const App = () => {
       }}
     >
       <h1 style={{ color: '#3645ab' }}>Phonebook</h1>
-      <ContactForm onSubmit={handleAddContact} />
+      <ContactForm />
       <h2>Contacts</h2>
-      <Filter value={filter} onChange={changeFilter} />
-      <ContactList
-        contacts={getFilteredContacts()}
-        onDeleteContact={handleDeleteContact}
-      />
+      <Filter />
+      <ContactList />
     </div>
   );
 };
--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -18,3 +18,34 @@ max-width: 400px;
   &:hover {
     transform: scale(1.05);
   }`;
+
+export const CardWrapper = styled.div`
+  display: flex;
+  height: 100px;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  justify-content: space-between;
+`;
+
+export const Info = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: -0.3px;
+`;
+
+export const ButtonDelete = styled.button`
+  width: 70px;
+  height: 40px;
+  margin-left: 5px;
+  border-width: inherit;
+  border-radius: 5px;
+  outline: none;
+  background-color: #e22626;
+  color: white;
+  cursor: pointer;
+
+  &:focus {
+    background-color: #c72323;
+  }
+`;
